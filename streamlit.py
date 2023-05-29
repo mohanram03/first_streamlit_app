e468c4f775ae1fc3813b0bea26e6ebee10b3c68a
mohanram03 <57432106+mohanram03@users.noreply.github.com>
2023-05-29 14:03:11 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,3 +2,8 @@ import streamlit
 
 streamlit.title('Hello StreamLit')
 streamlit.header('Welcome Lumen')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
